27933d17bf4724c5c1352357c52b963fe430112d
RagdIR <ragdirstrogov@gmail.com>
2020-08-29 12:27:50 +0600

    добавлены тестовые данные, сделаны фикстуры

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=300, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Создания опроса")
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Choice(models.Model):
+    variant = models.CharField(max_length=300, null=False, blank=False, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', related_name='poll',
+                               on_delete=models.PROTECT, verbose_name='Вопрос')
+
+    def __str__(self):
+        return self.variant
+
+    class Meta:
+        verbose_name = 'Текст варианта'
+        verbose_name_plural = 'Тексты вариантов'
